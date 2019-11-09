04ea6936e017bd23c09dd722f4d19c1fd1cb6639
rowro <rowro.dev@gmail.com>
2019-11-09 21:30:03 +0300

    feat: make layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './styles/main.scss';
-import 'material-design-lite/material';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.querySelector('#canvas');
+  const ctx = canvas.getContext('2d');
+
+  ctx.fillRect(0, 0, 512, 512);
+});
